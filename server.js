56b38498b5f328b500754b9f5811beed1fc3a43f
Robert Fodor <robertfodor.job@gmail.com>
2022-04-27 20:54:46 +0200

    10 Routes and Controllers Structure

--- server.js
+++ server.js
@@ -3,20 +3,29 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// db and authenticateUser
 import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-const port = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
